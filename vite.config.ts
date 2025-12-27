91c10d61e7f5fba7f74c18fd12b9787d838e7593
edubytes-vasconcelos <edubytes@gmail.com>
2025-12-27 00:26:23 -0300

    Remove PWA plugin from Vite configuration
    Removed PWA plugin configuration from Vite setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   root: "client",
   publicDir: "public",
 
   plugins: [
-    react(),
-    VitePWA({
-      registerType: "prompt",
-      devOptions: {
-        enabled: true
-      },
-      manifest: false
-    })
+    react()
   ]
 });
