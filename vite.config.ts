cd91370cd694818e27bcafbc522e0092a97f2604
Eduardo (Vasco) | Signa <vasconcelos@signainfo.com.br>
2025-12-27 02:09:55 -0300

    fix: add @ alias for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: "client",
-  publicDir: "public",
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-    },
+      "@": path.resolve(__dirname, "client/src")
+    }
   },
-  plugins: [react()],
+
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true
+  },
+
+  publicDir: "public",
+
+  plugins: [react()]
 });
