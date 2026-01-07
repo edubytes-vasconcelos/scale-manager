dcc376af166728688ddec3cfaf9a29b9fa1e3a1a
Eduardo (Vasco) | Signa <vasconcelos@signainfo.com.br>
2026-01-07 02:36:39 -0300

    Fix static serving root

--- server/static.ts
+++ server/static.ts
@@ -3,17 +3,18 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
-  if (!fs.existsSync(distPath)) {
+  const distPath = path.resolve(__dirname);
+  const indexPath = path.resolve(distPath, "index.html");
+
+  if (!fs.existsSync(indexPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build output. Expected index.html under ${distPath}, make sure the client is built before starting the server.`,
     );
   }
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(indexPath);
   });
 }
